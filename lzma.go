1d473f32005f6ffd81a354b5b8e03a3c211d47df
xuhua <cxuhua@gmail.com>
2020-06-18 16:39:17 +0800

    add level

--- lzma.go
+++ lzma.go
@@ -25,16 +25,20 @@ func Uncompress(src []byte) ([]byte, error) {
 	return ret, nil
 }
 
-func Compress(src []byte, level int) ([]byte, error) {
+func Compress(src []byte, level ...int) ([]byte, error) {
 	l := len(src)
 	if l == 0 {
 		return nil, errors.New("src data error")
+	}
+	lv := 5
+	if len(level) > 0 {
+		lv = level[0]
 	}
 	var srcptr *C.char = (*C.char)(unsafe.Pointer(&src[0]))
 	var srclen C.int = C.int(C.cxLzmaGetCompressLen(C.int(l)))
 	var ret []byte = make([]byte, int(srclen))
 	var dstptr *C.char = (*C.char)(unsafe.Pointer(&ret[0]))
-	if C.cxLzmaCompress(srcptr, C.int(l), dstptr, &srclen, C.int(level)) == nil {
+	if C.cxLzmaCompress(srcptr, C.int(l), dstptr, &srclen, C.int(lv)) == nil {
 		return nil, errors.New("compress failed")
 	}
 	return ret[0:int(srclen)], nil
